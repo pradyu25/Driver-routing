1e0a0ad15fd35e1fecc46d1a4f36e601f1054b4b
Pradyu25 <mprc9125@gmail.com>
2026-02-07 23:02:25 +0530

    update commit

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,6 +12,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-dev-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
+MAP_API_KEY = os.environ.get('MAP_API_KEY')
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
