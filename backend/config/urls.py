e196c101ecdceb1ce1717e48864fc84cd1659680
Pradyu25 <mprc9125@gmail.com>
2026-02-08 21:26:31 +0530

    update

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
+    path('', health_check),
     path('admin/', admin.site.urls),
     path('api/trips/', include('trips.urls')),
 ]
